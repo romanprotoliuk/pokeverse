47f561ec5bed607aaa845ce69fb52cb79b4489f8
romanprotoliuk <romanprotoliuk@gmail.com>
2023-05-14 20:44:34 -0700

    clean up

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import Image from "react-bootstrap/Image";
 
 const Navigation = () => {
   return (
